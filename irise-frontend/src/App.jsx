31e874b0264992644df0c94c59aa813298bcb94e
pratu52kule <132321426+pratu52kule@users.noreply.github.com>
2023-06-07 23:18:13 +0530

    reactcounter

--- irise-frontend/src/App.jsx
+++ irise-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Kids from './components/Kids2'
 import Mens from './components/Mens2';
 import Women from './components/Women2';
+import Home from './components/Home';
 import { Route,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route  path='/Kids' element={<Kids />}/>
         <Route  path='/Mens' element={<Mens />}/>
         <Route  path='/Women' element={<Women />}/>
--- irise-frontend/src/components/Mens2.jsx
+++ irise-frontend/src/components/Mens2.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 
-
-const Mens = () =>{
+const Mens = ()=>{
+    const[number,setnumber]=useState(9876);
+    const [isliked,setisliked]=useState(true);
+   // console.log(number,"number")
+   function counter(){
+    setnumber((prevstate)=>prevstate +1)
+   }
+   function decrement(){
+    setnumber((prevstate)=>prevstate-1)
+   }
+   function like(){
+    setisliked(false);
+    alert("liked");
+   }
     return(
-        <div>Mens</div>
+        <div>
+            <h1>Mens</h1>
+        <h3>counter : {number}</h3>
+        <button onClick={counter} >+</button>
+        <button onClick={decrement}>-</button>
+        {isliked ? <h2>not liked!</h2>:<h2>liked</h2>}
+        
+        <button onClick={like}>like</button>
+        </div>
     )
 }
 export default Mens;
