6bfe9f4be557514fed9a43b9f415481037353d50
pratu52kule <132321426+pratu52kule@users.noreply.github.com>
2023-06-30 13:24:03 +0530

    update

--- irise-frontend/src/components/Product.jsx
+++ irise-frontend/src/components/Product.jsx
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from 'react'
 
 const Product = () => {
     const [allproducts,setallproducts] = useState([]);
+    console.log(allproducts,"allproducts")
     useEffect(() => {
-        localStorage.getItem("product")
-    },[])
+      const data = JSON.parse (localStorage.getItem("products"));
+      setallproducts(data)
+        },[])
   return (
     <div>
-      Product
+     <h1>Products</h1> 
+     <div style={{display:'flex',justifyContent:'space-around',flexWrap:'wrap'}}>
+      {allproducts && allproducts.map((Product) => (
+        <div style={{border:'2px solid black',width:'30%' , marginBottom:'20px',height:'300px'}}>
+          <img style={{height:'200px'}} src={Product.image} alt='product image'/>
+          <h3>Name :{Product.name}</h3>
+          <h4>Price :{Product.price}</h4>
+        </div>
+     ) )}
+     </div>
     </div>
   )
 }
